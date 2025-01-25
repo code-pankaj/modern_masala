821922939d6abd55d64ec289fac64e16301b9740
code-pankaj <pankajyadavpy069@gmail.com>
2025-01-25 20:34:38 +0530

    back to zero

--- src/assets/images.js
+++
@@ -1,9 +0,0 @@
-const images = {
-    pancake:"https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    noodles:"https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    pizza:"https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    burger:"https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    coffee:"https://images.pexels.com/photos/851555/pexels-photo-851555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-}
-
-export default images;
